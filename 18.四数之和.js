b74a606724f188c8593518e48c7fbb0bd665cd6e
linyonglu <378617409@qq.com>
2023-12-20 17:09:25 +0800

    增加三数四数之和

--- 18.四数之和.js
+++ 18.四数之和.js
@@ -1,15 +1,47 @@
 /*
- * @lc app=leetcode.cn id=15 lang=javascript
+ * @lc app=leetcode.cn id=18 lang=javascript
  *
- * [15] 三数之和
+ * [18] 四数之和
  */
 
 // @lc code=start
 /**
  * @param {number[]} nums
+ * @param {number} target
  * @return {number[][]}
  */
-var threeSum = function (nums) {
+var fourSum = function (nums, target) {
+
+    //思路和三数之和一样
+
+    const len = nums.length;
+    const res = [];
+
+    //先排序
+    nums.sort((a, b) => a - b);
+
+
+    //遍历数组的每个元素
+    //固定一位，从剩下的元素中找到合为-nums[i]的，有就添加进结果数组中
+
+    for (let i = 0; i < len; i++) {
+        //固定一位，并且去除重复元素
+
+        const threesum = threeSum(nums, i + 1, target - nums[i]);
+
+        if (threesum.length > 0) {
+            for (let item of threesum) {
+                res.push([nums[i], ...item]);
+            }
+        }
+
+        while (i < len - 1 && nums[i] === nums[i + 1]) i++;
+    }
+
+    return res;
+};
+
+var threeSum = function (nums, start, target) {
 
     const len = nums.length;
     const res = [];
@@ -20,10 +52,10 @@ var threeSum = function (nums) {
     //遍历数组的每个元素
     //固定一位，从剩下的元素中找到合为-nums[i]的，有就添加进结果数组中
 
-    for (let i = 0; i < len; i++) {
+    for (let i = start; i < len; i++) {
         //固定一位，并且去除重复元素
 
-        const twosum = twoSum(nums, i + 1, 0 - nums[i]);
+        const twosum = twoSum(nums, i + 1, target - nums[i]);
 
         if (twosum.length > 0) {
             for (let item of twosum) {
@@ -39,11 +71,11 @@ var threeSum = function (nums) {
 
 //两数之和
 /**
- * 
- * @param {*} nums 
- * @param {*} start 
- * @param {*} target 
- */
+* 
+* @param {*} nums 
+* @param {*} start 
+* @param {*} target 
+*/
 var twoSum = function (nums, start, target) {
     //排序
     nums.sort((a, b) => a - b);
